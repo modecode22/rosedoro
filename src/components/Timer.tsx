c867fe1601d57818216e9f5127a58869ca883ae1
moncef aissaoui <108152453+modecode22@users.noreply.github.com>
2023-02-17 21:28:44 +0100

    timer update

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,22 +1,109 @@
+import { useEffect, useState } from "react";
+import { CircularProgressbar } from "react-circular-progressbar";
 
 const Timer = () => {
+  const session = 1500
+  const [seconds, setSeconds] = useState(session);
+  const [isRunning, setIsRunning] = useState(false);
+
+  useEffect(() => {
+    let intervalId: NodeJS.Timeout | undefined;
+    if (isRunning) {
+      intervalId = setInterval(() => {
+        setSeconds((currentSeconds) => {
+          if (currentSeconds === 0) {
+            clearInterval(intervalId!);
+            setIsRunning(false);
+            return session;
+          }
+          return currentSeconds - 1;
+        });
+      }, 1000);
+    }
+    return () => clearInterval(intervalId!);
+  }, [isRunning]);
+
+  const handleStart = () => setIsRunning(true);
+  const handlePause = () => setIsRunning(false);
+  const handleReset = () => {
+    setSeconds(session);
+    setIsRunning(false);
+  };
+
+  const minutes = Math.floor(seconds / 60);
+  const formattedSeconds = `${seconds % 60}`.padStart(2, "0");
+
+
+
   return (
     <div className="p-2 gap-2 flex flex-col justify-center items-center w-full">
-      <div className="select-none text-4xl group gap-2 bg-black/40  w-40 h-20  rounded-full border-2 border-rose-100/20 shadow-none shadow-rose-900/20 hover:shadow-lg hover:border-rose-100/60 duration-100 p-1 flex justify-center items-center">
-        <div>12</div>
-        <div>:</div>
-        <div>00</div>
+      <div className="w-28">
+        <CircularProgressbar
+          value={(seconds * 100) / session}
+          text={`${minutes}:${formattedSeconds}`}
+        />
       </div>
-      <div className="text-rose-50/80 hover:text-rose-50 text-2xl w-40 h10 group gap-2 bg-black/40   h-10  rounded-full border-2 border-rose-500/20 shadow-none shadow-rose-900/20 hover:shadow-lg hover:shadow-rose-900/40 duration-100 hover:border-rose-500/60  p-1 flex justify-center items-center active:border-rose-400 active:shadow-rose-400/50 active:text-white select-none cursor-pointer">
-        Start
-      </div>
-      <div className="w-40 flex items-center justify-center gap-2 ">
-        <div className="text-rose-50/80 hover:text-rose-50 text-xl w-full h10 group gap-2 bg-black/40   h-10  rounded-full border-2 border-rose-500/20 shadow-none shadow-rose-900/20 hover:shadow-lg hover:shadow-rose-900/40 duration-100 hover:border-rose-500/60  p-1 flex justify-center items-center active:border-rose-400 active:shadow-rose-400/50 active:text-white select-none cursor-pointer">
-          Pause
-        </div>
-        <div className="text-rose-50/80 hover:text-rose-50 text-xl w-full h10 group gap-2 bg-black/40   h-10  rounded-full border-2 border-rose-500/20 shadow-none shadow-rose-900/20 hover:shadow-lg hover:shadow-rose-900/40 duration-100 hover:border-rose-500/60  p-1 flex justify-center items-center active:border-rose-400 active:shadow-rose-400/50 active:text-white select-none cursor-pointer">
-          Reset
-        </div>
+
+      <div className=" flex items-end justify-center gap-2 ">
+        <button
+          onClick={handlePause}
+          disabled={!isRunning}
+          className="transition-all disabled:text-rose-50/80 text-rose-50/90 hover:text-rose-50 text-xl w-8 group gap-2 disabled:bg-black/40 bg-black/50   h-8  rounded-full border-2 disabled:border-rose-500/20 border-rose-500/40 shadow-none shadow-rose-900/20 hover:shadow-lg hover:shadow-rose-900/40 duration-100 hover:border-rose-500/60  p-1 flex justify-center items-center active:border-rose-400 active:shadow-rose-400/50 active:text-white select-none cursor-pointer"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M15.75 5.25v13.5m-7.5-13.5v13.5"
+            />
+          </svg>
+        </button>
+
+        <button
+          onClick={handleStart}
+          disabled={isRunning}
+          className="transition-all disabled:text-rose-50/80 text-rose-50/90 hover:text-rose-50 text-2xl w-10 group gap-2 disabled:bg-black/40 bg-black/50   h-10  rounded-full border-2 disabled:border-rose-500/20 border-rose-500/40 shadow-none shadow-rose-900/20 hover:shadow-lg hover:shadow-rose-900/40 duration-100 hover:border-rose-500/60  p-1 flex justify-center items-center active:border-rose-400 active:shadow-rose-400/50 active:text-white select-none cursor-pointer"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.348a1.125 1.125 0 010 1.971l-11.54 6.347a1.125 1.125 0 01-1.667-.985V5.653z"
+            />
+          </svg>
+        </button>
+
+        <button
+          onClick={handleReset}
+          className="transition-all disabled:text-rose-50/80 text-rose-50/90 hover:text-rose-50 text-xl w-8 group gap-2 disabled:bg-black/40 bg-black/50   h-8  rounded-full border-2 disabled:border-rose-500/20 border-rose-500/40 shadow-none shadow-rose-900/20 hover:shadow-lg hover:shadow-rose-900/40 duration-100 hover:border-rose-500/60  p-1 flex justify-center items-center active:border-rose-400 active:shadow-rose-400/50 active:text-white select-none cursor-pointer"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          >
+            <path d="M3 2v6h6"></path>
+            <path d="M3 13a9 9 0 1 0 3-7.7L3 8"></path>
+          </svg>
+        </button>
       </div>
     </div>
   );
